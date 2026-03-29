1ca4dc1bc89aad8a8f6cc71a95512c8428918367
ikyStarboy <fadrygaming3@gmail.com>
2026-03-30 03:41:33 +0700

    Update StreamView.tsx

--- views/StreamView.tsx
+++ views/StreamView.tsx
@@ -519,7 +519,7 @@ export default function StreamView({ chapterUrlId, onBack }: { chapterUrlId: str
                       {c.user_name}
                       {c.role === 'own' && <img src="/assets/icons/centang.png" alt="Verified" className="w-3.5 h-3.5 object-contain" onError={(e: any) => e.target.style.display='none'} />}
                       {c.role === 'admin' && <img src="/assets/icons/centangmr.png" alt="Admin" className="w-3.5 h-3.5 object-contain" onError={(e: any) => e.target.style.display='none'} />}
-                      {c.active_title && <img src={`/assets/title/${c.active_title}.png`} className="w-16 sm:w-20 ml-1.5 object-contain translate-y-[2px]" onError={(e: any) => e.target.style.display='none'} />}
+                      {c.active_title && <img src={`/assets/title/${c.active_title}.png`} className="w-24 sm:w-28 ml-1.5 object-contain translate-y-[2px]" onError={(e: any) => e.target.style.display='none'} />}
                     </span>
                     <span className="text-[#666] text-[11px] whitespace-nowrap">
                       {new Date(c.created_at).toLocaleDateString('id-ID', {day: 'numeric', month: 'short'})}
